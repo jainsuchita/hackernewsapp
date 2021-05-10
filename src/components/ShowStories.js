b7d7de9903f527315684fd89d1731e35be1feba2
jainsuchita <jainsuchita38@gmail.com>
2021-05-10 13:14:16 +0530

    add Hacker news clone

---
+++ src/components/ShowStories.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Story from "./Story";
+import useDataFetcher from "../hooks/DataFetcher";
+import Loader from "./Loader";
+
+const ShowStories = ({ type }) => {
+  const { isLoading, stories } = useDataFetcher(type ? type : "top");
+
+  return (
+    <>
+      <Loader show={isLoading}>Loading...</Loader>
+      <>
+        {stories.map(({ data: story }) => (
+          <Story key={story.id} story={story} />
+        ))}
+      </>
+      )}
+    </>
+  );
+};
+
+export default ShowStories;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import LessText from "./LessText";
+// import "./index.css";
+// import App from "./components/App/App";
+// import LessText from "./components/App/LessText";
+import AppRouter from "./router/AppRouter";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles.scss";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-    <LessText
+    {/* <App /> */}
+    {/* <LessText
       text={`Focused, hard work is the real key
       to success. Keep your eyes on the goal, 
       and just keep taking the next step 
       towards completing it.`}
       maxLength={35}
-    />
+    /> */}
+    <AppRouter />
   </React.StrictMode>,
   document.getElementById("root")
 );
